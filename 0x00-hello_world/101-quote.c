5540242c834b0fd091fad64d639d9af1030947f7
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-05 16:45:40 -0700

    Added C file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	/*printing characters using char pointer msg*/
 	const char *part1 = "and that piece of art is useful\"";
-	const char *part2= " - Dora Korpari, 2015-10-19";
+	const char *part2= " - Dora Korpar, 2015-10-19\n";
 	
 	/*file descriptor*/
 	write(2, part1, strlen(part1));
