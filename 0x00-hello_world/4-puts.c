78b055f9c036a0b32fb90d79b33e089e303c2588
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-05 08:46:20 -0700

    Added file puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main(void) /* The program starts execution from here */
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
