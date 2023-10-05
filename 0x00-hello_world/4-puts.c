1612d3116dc1a83e1d59922e0800792b4d5cc53e
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-05 08:50:06 -0700

    Added file puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 
 int main(void) /* The program starts execution from here */
 {
-	puts("\"Programming is like building a multilingual puzzle");/*Prints outs the string shown*/
+	puts("\"Programming is like building a multilingual puzzle");/*Prints string*/
 	return (0);
 }
