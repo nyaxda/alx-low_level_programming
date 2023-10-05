ed27b83633f175760f80b5d22ef14ddd9332808e
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-05 10:36:50 -0700

    Added C file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry Point.
+ *
+ * Description: Prints content followed by new line so stderr
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*printing characters using char pointer message*/
+	const char *goo = "and that piece of art is useful\n" - Dora Korpar, 2015-10-19"
+	write(2, message, srtlen(goo);
+	return 1;
+}
