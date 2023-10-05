a550c76f114e7cc510f86e8ffc8d64cc447f1a8e
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-05 11:25:15 -0700

    Added C file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
 int main(void)
 {
 	/*printing characters using char pointer msg*/
-	const char *goo = "and that piece of art is useful\" - Dora Korpar, \n"
+	const char *goo = "and that piece of art is useful\" - Dora Korpar, \"
 		"2015-10-19\n";
 
 	write(2, goo, strlen(goo));
