e761c420198f8d29cfa862882c4d2682dc82fdaa
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-06 12:34:04 -0700

    Added C file 3-prints_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description - prints alphabet in lowercase & UPPERCASE
+ * followed by new line.
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	int i;
+	int a = strlen(alphabet);
+
+	for (i = 0; i < a; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	for ( i = 0; i < a; i++);
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
