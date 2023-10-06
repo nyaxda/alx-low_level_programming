7c722f10961da3716f9de14f15ae558bee9a5dc7
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-06 13:38:39 -0700

    Added C file 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description - prints alphabet in lowercase in reverse followed by new line.
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int a = strlen(alphabet);
+
+	for (i = a; i > 0; i--)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
