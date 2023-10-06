0fb2869741b1a83b149a0aa371417de5687ca8a4
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-06 12:42:17 -0700

    Added C file 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description - prints alphabet in lowercase followed by new line.
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+	int a = strlen(alphabet);
+
+	for (i = 0; i < a; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
