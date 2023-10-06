e9221adb99e7560ac55daca7bca1f73b1ebc714e
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-06 10:35:57 -0700

    Added C file 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry Point.
+ *
+ * Description: print nature of the number stored in the variable n 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
