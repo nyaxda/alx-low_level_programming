efc15cc4a24a4b29535af89be4de94680a8e5847
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-11 14:36:41 -0700

    Added C file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point.
+ *
+ * Description: prints '_putchar' with a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+
+	return (0);
+}
