2871c2f8a785718aa01b2bbd0875b8cf87c47872
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-11 16:32:49 -0700

    Added C file 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * print_alphabetx10 - Entry Point.
+ * print_alphabet_x10 - Entry Point.
  *
- * Description: Function print_alphabetx10 printing lowercase letters x10.
+ * Description: Function print_alphabet_x10 printing lowercase letters x10.
  *
  * Return: Always 0 (Success)
  */
@@ -12,8 +12,9 @@
 void print_alphabet_x10(void) /*Function definition*/
 {
 	int i = 0;
-	while (i<10)
-	{	
+
+	while (i < 10)
+	{
 		char beg = 'a';
 		
 		while (beg <= 'z')
