4f19250a405241c95b6d2b0043f9bd4d3d5917be
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-11 16:05:27 -0700

    Added file C 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point.
+ *
+ * Description: Prints alphabet in lowercase, then newline
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char beg = 'a';
+	while (beg <= 'z')
+	{
+		_putchar(beg);
+		beg++;
+	}
+
+	_putchar(10)
+}
