2d4b44901d51da846c95528ac0897d5fbba79952
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-12 16:57:55 -0700

    Added C file 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - draws a straight diagonal in the terminal.
+ *
+ * Description: function that draws a straight diagonal in the terminal.
+ *
+ *@n: Variable to check
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			int j = 0;
+
+			while (j < i)
+			{
+				putchar(32);
+				j++;
+			}
+			putchar(92);
+			putchar('\n');
+		}
+	}
+	else
+		putchar('\n');
+}
