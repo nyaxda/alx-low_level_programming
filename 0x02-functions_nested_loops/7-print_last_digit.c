5a425e80a90ba5e8fa6a7f3d9476785ede33af37
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-11 17:50:12 -0700

    Added C file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry Point.
+ *
+ * Description: Function that prints last digit of a number.
+ *
+ * @r: the character to check
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int)
+{
+	return(r%10);
+}
