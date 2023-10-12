7fbc4952bce23052bb897e13c61e732540b3e3a2
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-11 18:18:38 -0700

    Added C file 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Entry Point.
+ *
+ * Description: Function that prints every minute from 00:00 to 23:59
+ *@h, @m: characters to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+	for (h = 0, h < 24, h ++)
+	{
+		for (m = 0, m <60, m ++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_puthar('\n')
+		}
+	}
+}
+	return(0);
+}
