31a8c19d2bf30319005e1df26a132b1125a80516
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-11 18:29:53 -0700

    Added C file 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,9 +14,10 @@ void jack_bauer(void)
 {
 	int h;
 	int m;
+
 	for (h = 0; h < 24; h++)
 	{
-		for (m = 0; m <60; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar('0' + (h / 10));
 			_putchar('0' + (h % 10));
