968ff0021a8a2c02704c4f305d8e18f4371cb25f
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-13 05:06:22 -0700

    Added c file 0-main.c, header file main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -14,15 +12,15 @@ void positive_or_negative(int i)
 {
 	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 
 	return (0);
