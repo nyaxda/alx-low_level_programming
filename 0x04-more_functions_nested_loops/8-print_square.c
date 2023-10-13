c10c5ca88f9528115166125f8c08eac70ee7852d
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-12 17:35:33 -0700

    Added C file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - draws a square in the terminal.
+ *
+ * Description: function that draws a square in the terminal.
+ *
+ *@size: Variable to check
+ *
+ * Return: void
+ */
+
+void print_square(int size);
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+		}
+	else
+	{
+		int i = 0;
+
+		while (i < size)
+		{
+			int j = size;
+
+			for (; j > 0; j--)
+			{
+				_putchar(35);
+			}
+			_putchar ('\n');
+			i++;
+			}
+		_putchar('\n');
+	}
+}
