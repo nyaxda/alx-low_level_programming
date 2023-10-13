581970b3f3766eac5f1b37d0df647b702335a3ec
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-12 18:27:14 -0700

    Added C file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0 && !(i < 15))
+		if (i % 15 == 0 && i >= 15)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 3 == 0 && !(i < 3))
+		else if (i % 3 == 0 && i >= 3)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0 && !(i < 5))
+		else if (i % 5 == 0 && i >= 5)
 		{
 			printf("Buzz");
 		}
