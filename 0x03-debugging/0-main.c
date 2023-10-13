71288b1c06e7e43081042237a458bc65a10872bd
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-13 06:10:02 -0700

    Added c file 0-main.c, header file main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry Point.
+ * main - tests funtion that prints if integer is positive or negative.
  *
  *Description: Tests for 0 if it's positive or negative
  *
@@ -9,6 +9,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
