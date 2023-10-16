57967d94d9b8d3022d31633a03f7e12479069323
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-16 11:30:28 -0400

    Error fix on 3-puts.c file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * swap_int - prints a string, followed by a new line, to stdout.
+ * _puts - prints a string, followed by a new line, to stdout.
  *
  * Description: function that prints a string,then a new line, to stdout.
  *
@@ -14,8 +14,9 @@ void _puts(char *str)
 {
 	size_t i;
 
-	for (i= 0; i < strlen(str); i++)
+	for (i = 0; i < strlen(str); i++)
 	{
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
