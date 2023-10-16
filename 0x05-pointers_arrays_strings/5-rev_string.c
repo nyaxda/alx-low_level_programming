e4212f34deaf5643abf6b1962d2695b76748fdb3
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-16 12:54:28 -0400

    Added C file "5-rev-string.c"

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,29 @@
 #include <string.h>
-#include "main.h"
-
+#include <stdio.h>
 /**
  * rev_string - prints a string in reverse.
  *
  * Description: function that prints a string in reverse.
  *
- * @s: pointer to a char
+ *@i: integer variable
+ *@t: integer variable
+ *@len: long integer variable
  *
  * Return: void
  */
+void rev_string(char *s);
 void rev_string(char *s)
 {
-	size_t i;
+	int i;
+	int t;
+	size_t len;
+	len = strlen (s);
 
-	i = (strlen(s));
-
-	while (i--)
+	for (i = 0; i < len / 2; i++)
 	{
-		_putchar(s[i]);
+		t = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = t;
 	}
+	return (s);
 }
