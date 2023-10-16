b689227cd9df0254756f322926090a1707cc04d9
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-16 14:05:30 -0400

    Error fix for C file "7-puts_half.c"

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,14 +16,14 @@ void puts_half(char *str)
 
 	if (strlen(str) % 2  == 0)
 	{
-		for (i = (strlen(str) / 2); i < (strlen(str)); i++)
+		for (i = (strlen(str) / 2); i < ((strlen(str))); i++)
 		{
 			_putchar(str[i]);
 		}
 	}
-	else 
+	else
 	{
-		for (i = ((strlen(str)) / 2); i < (strlen(str)); i++)
+		for (i = ((strlen(str) + 1) / 2); i < (strlen(str)); i++)
 		{
 			_putchar(str[i]);
 		}
