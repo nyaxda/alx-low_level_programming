c7c138d73a9679304404dfdd72efa89cc0208ced
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-16 17:29:26 -0400

    Error fix for "9-strcpy.c"

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- **_strcpy - copies the string pointed to by src, including the 
+ **_strcpy - copies the string pointed to by src, including the
  *terminating null byte (\0), to the buffer pointed to by dest.
  *Description: a function that copies the string pointed to by src to dest.
  *
