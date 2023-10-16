45f09345dcd66dfd4f0b40e5dd973a3739d75422
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-16 13:34:53 -0400

    Added C file "6-puts2.c"

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character, starting with 1st character.
+ * Description: function that prints every other xter,then a new line.
+ *
+ * @str: pointer to a char
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+    size_t i;
+
+	for (i = 0; i < strlen(str); i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
