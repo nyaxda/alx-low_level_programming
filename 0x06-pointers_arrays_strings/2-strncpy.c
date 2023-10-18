8238ba76c84e415c1b4601e42e9fdb2af280eb53
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-18 07:33:16 -0400

    Added C file "2-strncpy.c"

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * Description: function that copies a string.
+ * 
+ *@dest: pointer to a char
+ *@src: pointer to a char
+ *@n: integer variable
+ *
+ * Return: concantenated string (char*)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *origin = dest;
+
+	while (n > 0 && *src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		n--
+	}
+	return (origin);
+}
