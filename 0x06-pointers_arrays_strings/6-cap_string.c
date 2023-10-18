ee4e5f3479582038fbcded8cc1aab3f43f7f68d1
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-18 16:53:12 -0400

    Added C file "6-cap_string.c"

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes first words of a string.
+ * Description: function that capitalizes first words of a string.
+ *
+ * Return: capitalized first words of a string (*char).
+ */
+
+char *cap_string(char *ptr)
+{
+	char *origin = ptr;
+	size_t i;
+	int j;
+
+	*origin = *ptr;
+
+	while (*origin != '\0')
+	{
+        i = (int)*origin;
+        if (*origin == ' ' ||
+        *origin == ',' ||
+        *origin == ';' ||
+        *origin == '!' ||
+        *origin == '?' ||
+        *origin == '"' ||
+        *origin == '(' ||
+        *origin == ')' ||
+        *origin == '{' ||
+        *origin == '}'
+        )
+        {
+            *origin = ' ';
+        }
+        if ( *(origin - 1) == ' ' ||
+        *(origin - 1) == '\n' ||
+        *(origin - 1) == '\t' ||
+        *(origin - 1) == '.')
+        {
+		    if (i > 96 && i < 123)
+		    {
+			    *origin = ((char)(i - 32));
+		    }
+            else
+            {
+                *origin = ((char)i);
+            }
+        }
+		origin++;
+	}
+	return (ptr);
+}
