0fa8fb3d075a177278a29f6456f6d6adae14118e
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-19 08:28:58 -0400

    Added C file "7-leet.c"

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <string.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * Description: function that encodes a string into 1337.
+ *
+ *@ptr: pointer to a char.
+ *
+ * Return: capitalized first words of a string (*char).
+ */
+
+char *leet(char *ptr)
+{
+	int i;
+	char mod[128];
+	char *init = ptr;
+
+	for (i = 0; i < 128; i++)
+	{
+		mod[i] = (char)i;
+	}
+
+	mod['a'] = '4';
+	mod['e'] = '3';
+	mod['o'] = '0';
+	mod['t'] = '7';
+	mod['l'] = '1';
+
+	ptr = init;
+
+	while (*ptr != '\0')
+	{
+		i = (int)*ptr;
+		if (mod[i] != '\0')
+		{
+			*ptr = mod[i];
+		}
+		ptr++;
+	}
+	return (init);
+}
