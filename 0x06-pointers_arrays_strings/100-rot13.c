6cf8aecd57a717c7a1f8e780133b2919ec68c56b
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-19 15:23:49 -0400

    Added C file "100-rot13.c"

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * Description: function that encodes a string using rot13.
+ *
+ *@str: pointer to a char.
+ *
+ * Return: encoded string (*char).
+ */
+char *rot13(char *str)
+{
+	size_t i;
+	char *init = str;
+	i = (int)(*str);
+	while (*str != '\0')
+	{
+		i = (int)(*str);
+		if (isalpha(*str))
+		{
+			if ((i >= 65 && i <= 77 ) || (i >= 97 && i <= 109))
+			{
+				*str = (char)(i + 13);
+			}
+			else
+			{
+				*str = (char)(i - 13);
+			}
+		}
+		str++;
+	}
+	return (init);
+}
