5d7f5b381a728ac5107d05f7c48c732b888f431d
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-23 14:38:35 -0400

    Hotfix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,57 +16,72 @@
  * or NULL if the substring is not found.
  *
  */
-
 void print_diagsums(int *a, int size)
 {
-	if (size <= 0)
-	{
-		return;
-	}
-	int sum_1 = 0;
-	int sum_2 = 0;
+		int rows = size;
+		int cols = size;
+		int sum_1 = 0;
+		int sum_2 = 0;
+		int i, j;
+		int number;
+		int divisor = 1;
 
-	for (int i = 0; i < size; i++)
-	{
-		sum_1 += a[i * size + i];
-		sum_2 += a[i * size + (size - 1 - i)];
-	}
-	if (sum_1 < 0)
-	{
+		if (rows != cols)
+		{
+				return;
+		}
+
+		for (i = 0; i < rows; i++)
+		{
+				for (j = 0; j < cols; j++)
+				{
+						if (i == j)
+						{
+								sum_1 += a[i * cols + j];
+						}
+						if (i + j == rows - 1)
+						{
+								sum_2 += a[i * cols + j];
+						}
+				}
+		}
+if (sum_1 < 0)
+{
 		_putchar('-');
 		sum_1 = -sum_1;
-	}
-	int divisor = 1;
-	int number = sum_1;
-	while (divisor * 10 <= number)
-	{
+}
+number = sum_1;
+while (divisor * 10 <= number)
+{
 		divisor *= 10;
-	}
-	while (divisor > 0)
-	{
-		_putchar((number / divisor) + '0');
+}
+while (divisor > 0)
+{
+		_putchar ((number / divisor) + '0');
 		number %= divisor;
 		divisor /= 10;
-	}
-	_putchar(',');
-	_putchar(' ');
-	if (sum_2 < 0)
-	{
-		_putchar('-');
-		sum_2 = -sum_2;
-	}
-	divisor = 1;
-	number = sum_2;
-	while (divisor * 10 <= number)
-	{
-		divisor *= 10;
-	}
-	while (divisor > 0)
-	{
-		_putchar((number / divisor) + '0');
-		number %= divisor;
-		divisor /= 10;
-	}
-	_putchar('\n');
 }
 
+_putchar(',');
+_putchar(' ');
+
+if (sum_2 < 0)
+{
+		_putchar('-');
+		sum_2 = -sum_2;
+}
+
+number = sum_2;
+divisor = 1;
+while (divisor * 10 <= number)
+{
+		divisor *= 10;
+}
+while (divisor > 0)
+{
+		_putchar ((number / divisor) + '0');
+		number %= divisor;
+		divisor /= 10;
+}
+_putchar('\n');
+}
