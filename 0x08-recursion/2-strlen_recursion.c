511928d1083848c80c61ceac1adfed1b492d5af1
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-25 10:59:38 -0400

    Added C file "2-strlen_recursion.c"

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion -  returns the length of a string.
+ *
+ * Description: function that returns the length of a string.
+ *
+ *@s: pointer to a char
+ *
+ * Return: length of string (int)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
