dce62015795d47bfbd43df5d6b30d5ff4c9a30d0
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-25 19:46:01 -0400

    Hotfix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -23,7 +23,12 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
-		return (*s2 == '\0') ? 1 : 0;
+		if (*s2 == '\0')
+		{
+			return (1);
+		}
+		return (0);
+
 	}
 	if (*s2 == '*')
 	{
@@ -31,7 +36,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s1 == *s2)
 	{
-		return(wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
