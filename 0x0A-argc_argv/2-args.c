053caaa4cce4933db4c6e878044cc89522bfae68
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-27 13:19:28 -0400

    Added C file "2-args.c"

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives.
+ *
+ * Description: program that prints prints all arguments it receives.
+ *@argc: no. of arguments passed to the program
+ *@argv:argument vector of pointers to strings
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i = 0;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
