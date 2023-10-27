1dd4b14123707d926117e7aead1b45a39d8d73ef
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-27 12:41:01 -0400

    Added C file "1-args.c"

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it.
+ *
+ * Description: program that prints the number of arguments passed into it.
+ *
+ *@argc: no. of arguments passed to the program
+ *@argv:argument vector of pointers to strings
+ *
+ * Return: 0 on success
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	int i;
+	int a = strlen(argv[0]);
+
+	for (i = 0; i < a; i++)
+	{
+		putchar(argv[0][i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
