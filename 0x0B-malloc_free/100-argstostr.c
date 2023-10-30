2ed7f4362e34b8144f081e7a38a8a8201bd94cf4
Daniel Nyakundi <nyaxda@gmail.com>
2023-10-30 13:57:14 -0400

    Hotfix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,31 +23,13 @@ char *argstostr(int ac, char **av)
 	{
 		return (NULL);
 	}
+	if (ac < 2)
+
+	str = malloc(ac * sizeof(char) + 1);
 
 	for (i = 0; i < ac; i++)
 	{
-		j += strlen(av[i]) + 1;
+		str[i] = av[i];
 	}
-
-	str = (char *)malloc(j * sizeof(char) + 1);
-	
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	i = 0;
-	j = 0;
-
-	while (i < ac)
-	{
-		for (j = 0; j < strlen(av[i]); j++)
-		{
-			str[j] = av[i][j];
-		}
-		str[j] = '\n';
-		i++;
-	}
-	str[j] = '\0';
 	return (str);
 }
