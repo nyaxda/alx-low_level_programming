449591b624fb3d65496bd35c50eda295a3e21438
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-02 06:26:36 -0400

    Added C file "0-malloc_checked.c"

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked -  allocates memory using malloc.
+ *
+ * Description: function that callocates memory using malloc.
+ *
+ *@b: size of the memory to allocate.
+ *
+ * Return:  pointer to the allocated memory. if malloc fails,
+ * the malloc_checked function should
+ * cause normal process termination with a status value of 98
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *m = malloc(sizeof(b));
+	if (m == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (m);
+	}
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+
+#endif /* HOLBERTON_H */
