64aafc2683f7669f179d0a23ad3c9ff190e46c3d
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-06 09:34:42 -0500

    Added header file "dog.h"

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * strcut dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * 
+ * Description: dog structure
+*/
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+}
+
+#endif
