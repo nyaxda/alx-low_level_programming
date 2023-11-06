961b836c066c4961a33b4cc3d1767754354ece0c
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-06 07:56:29 -0500

    Added C file "#2-main.c"

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * Description: program that prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
