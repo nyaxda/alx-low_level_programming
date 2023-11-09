03fb529d57f6d08173ea89875b12b1a913ff4cff
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-09 12:18:37 -0500

    Hotfix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,41 @@
 #include "variadic_functions.h"
 
 /**
+ * print_variable - prints a variable.
+ *
+ * Description: function that prints a variable.
+ *
+ * @format: type of variable to print
+ * @args: list of arguments
+ *
+ * Return: void
+*/
+
+void print_variable(const char format, va_list args)
+{
+	switch (format)
+	{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			char *s = va_arg(args, char *);
+			if (!s)
+			{
+				printf("(nil)");
+			}
+			printf("%s", s);
+			break;
+	}
+}
+
+/**
  * print_all - prints anything.
  *
  * Description: function that prints anything.
@@ -14,7 +49,6 @@
 void print_all(const char * const format, ...)
 {
 	unsigned int j = 0, k;
-	char *s;
 	const char  variables[] = "cifs";
 	va_list args;
 
@@ -24,7 +58,7 @@ void print_all(const char * const format, ...)
 	{
 		k = 0;
 		while (variables[k])
-		{
+			{
 			if (format[j] == variables[k] && k > 0)
 			{
 				printf(", ");
@@ -32,26 +66,7 @@ void print_all(const char * const format, ...)
 			}
 			k++;
 		}
-		switch (format[j])
-		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (!s)
-				{
-					printf("(nil)");
-				}
-				printf("%s", s);
-				break;
-		}
+		print_variable(format[j], args);
 		j++;
 	}
 	printf("\n");
