e8939172a2bb88ba3edce42b01de6514dd1f11a5
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-09 06:44:30 -0500

    Hotfix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,14 +15,13 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
     unsigned int i;
+    va_list args;
+    va_start(args, n);
 
     if (separator == NULL)
     {
         separator = "";
     }
-
-    va_list args;
-    va_start(args, n);
 
     for (i = 0; i < n; i++)
     {
