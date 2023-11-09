281030b8f116ee43a4f9cd9ae1c8446e2b75a961
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-09 06:21:44 -0500

    Hotfix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,11 +16,13 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0, j;
 
-	va_start(args, n)
+	va_start(args, n);
 	j = va_arg(args, int);
 	
-	if (n == 0);
+	if (n == 0)
+	{
 		return (0);
+	}
 	for (i = 0; i < n; i++)
 	{
 		sum += j;
