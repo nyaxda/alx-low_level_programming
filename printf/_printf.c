a86af1a6161af357918497d5eb561680b533ba87
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-11 07:05:51 -0500

    Added C file "_printf.c"

---
+++ printf/_printf.c
@@ -0,0 +1,101 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_character - prints a character
+ * @args: character to print
+ * Return: number of characters printed
+ */
+int print_character(va_list args)
+{
+	char c;
+	c = va_arg(args, int);
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @arg s: string to print
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char*);
+	int len = 0;
+
+	if (str == NULL)
+	{
+		return (-1);
+	}
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		len ++;
+		str++;
+	}
+	return (len);
+}
+
+/**
+ * print_percent - prints a percent sign
+ * @args: percent sign to print
+ * Return: number of characters printed
+*/
+int print_percent(va_list args)
+{
+	(void)args;
+	write(1, "%", 1);
+	return (1);
+}
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int print = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while(*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			print++;
+		}
+		else
+		{
+			format++;
+			if (format == '\0')
+			{
+				break;
+			}
+			if (*format == 'c')
+			{
+				print += print_character(args);
+			}
+			else if (*format == 's')
+			{
+				print += print_string(args);
+			}
+			else if (*format == '%')
+			{
+				print += print_percent(args);
+			}
+			else
+			{
+				write(1, format, 1);
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (print);
+}
