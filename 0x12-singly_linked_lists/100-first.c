7facf6909470c59979014a1f5a04b83dc985490a
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-16 17:12:33 -0500

    Hotfix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor))
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
