b27e97ed1b4ff62567609920ab1b00348551b75f
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-16 17:19:39 -0500

    Function fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-void first(void) __attribute__ ((constructor))
+/**
+ * first - prints a string before main function.
+ *
+ * Return: void
+ */
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
