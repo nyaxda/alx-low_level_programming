ea18b87aaef367cfcede3f42b025a87bcc5671bb
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-21 13:09:09 -0500

    Fix issue with inserting node at specific index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,17 +17,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int index = 0;
 
 	if (*head == NULL)
-	{
 		return (NULL);
-	}
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	ptr->n = n;
 	ptr->next = NULL;
-
 	if (idx == 0)
 	{
 		ptr->next = *head;
@@ -57,6 +52,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	ptr->next = current->next;
 	current->next = ptr;
-
 	return (ptr);
 }
