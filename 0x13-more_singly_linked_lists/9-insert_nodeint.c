fd02dba27e3bf0b2f8e6554d0435de42273d4406
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-21 12:52:43 -0500

    Fix loop condition in insert_nodeint_at_index function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr = malloc(sizeof(listint_t));
 	current = *head;
 	ptr->n = n;
-	while(index < idx - 1 && current)
+	while (index < idx - 1 && current)
 	{
 		current = current->next;
 		index++;
