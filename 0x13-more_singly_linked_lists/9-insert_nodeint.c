b46bcf894f9e6d2e174a063fa8eb3a7f937063b9
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-21 12:54:55 -0500

    Fix return type comment in insert_nodeint_at_index function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  * @idx: indes of the list where the new node should be added.
  * @n: data of the node to be added.
  *
- * Return - address of the new node, or NULL if it failed.
+ * Return: address of the new node, or NULL if it failed.
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
