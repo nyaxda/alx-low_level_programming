664e716b241f79629a8c6cad547d64527b197262
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-21 14:38:35 -0500

    Added C file 102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a list
+ * @h: the pointer to the start of list to free
+ * Return: size of free'd list (in nodes?)
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *node;
+
+	if (h == NULL || *h == NULL)
+	{
+		return count;
+	}
+	while (*h != NULL)
+	{
+		if (*h > (*h)->next)
+		{
+			node = (*h)->next;
+			free(*h);
+			*h = node;
+		}
+		else
+		{
+			node = *h;
+			*h = NULL;
+			free(node);
+		}
+		count++;
+	}
+	return (count);
+}
