d056a3d86e914c331af909b4f05dde599b8708f7
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-21 14:31:23 -0500

    Reverse the linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,17 +10,18 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev_node = NULL;
-	listint_t *current_node = *head;
-	listint_t *next_node;
+	listint_t *ptr = NULL;
+	listint_t *nptr = NULL;
 
-	while (current_node != NULL)
+	while (*head)
 	{
-		next_node = current_node->next;
-		current_node->next = prev_node;
-		prev_node = current_node;
-		current_node = next_node;
+		nptr = (*head)->next;
+		(*head)->next = ptr;
+		ptr = *head;
+		*head = nptr;
 	}
-	*head = prev_node;
+
+	*head = ptr;
+
 	return (*head);
 }
