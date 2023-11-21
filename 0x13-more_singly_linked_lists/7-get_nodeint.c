c10b24c56b3785e430dc355119f7777cd6bbe70f
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-21 11:41:33 -0500

    Fix formatting and add missing parameter description in get_nodeint_at_index function

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,6 +6,7 @@
  * get_nodeint_at_index - gets the nth node of a listint_t linked list.
  *
  * @head: head pointer of the listint_t linked list.
+ * @index: position of the nth node.
  *
  * Return: nth node
 */
@@ -16,10 +17,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	ptr = head;
-	while(i < index)
+	while (i < index)
 	{
 		ptr = ptr->next;
 		i++;
