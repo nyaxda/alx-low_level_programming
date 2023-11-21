13316df75c818efb5e036801b713dfcf4e9975b7
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-21 11:44:02 -0500

    Fix index out of bounds bug in get_nodeint_at_index function

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,11 +20,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	}
 	ptr = head;
-	while (i < index)
+	while (i < index && ptr)
 	{
 		ptr = ptr->next;
 		i++;
 	}
-	ptr = ptr->next;
 	return (ptr);
 }
