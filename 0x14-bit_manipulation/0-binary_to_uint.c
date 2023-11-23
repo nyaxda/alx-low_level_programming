730353a44426a89822d75092661f2761a0b9f59c
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-23 13:50:34 -0500

    Fix binary_to_uint function and add _strcpy function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
+
+/**
+ **_strcpy - copies the string pointed to by src, including the
+ *terminating null byte (\0), to the buffer pointed to by dest.
+ *Description: a function that copies the string pointed to by src to dest.
+ *
+ *@dest: char pointer variable
+ *@src: char pointer variable
+ *
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	size_t i;
+
+	for (i = 0; i < strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[strlen(src)] = '\0';
+	return (dest);
+}
+
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * where b is pointing to a string of 0 and 1 chars
@@ -11,18 +35,26 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	char *error;
+	char *buffer;
 	unsigned int result;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-	while (*b != '\0')
+	buffer = malloc(strlen(b) + 1)
+	if (buffer == NULL)
 	{
-		if (*b != '0' && *b != '1')
+		return (0);
+	}
+
+	_strcpy(buffer, b);
+
+	while (*buffer != '\0')
+	{
+		if (*buffer != '0' && *buffer != '1')
 			return (0);
-		b++;
+		buffer++;
 	}
 	result = (unsigned int)strtoul(b, &error, 2);
 	if (error == b)
