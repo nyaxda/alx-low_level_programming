220006c9d95389c09bdb45901041ea7c9b9a8149
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-23 14:57:15 -0500

    Fix clear_bit function to correctly clear a bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,6 +12,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
-	*n |= (0 << index);
+	*n &= ~(1 << index);
 	return (1);
 }
