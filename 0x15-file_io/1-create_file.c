52298de8e49781b453905cdc1022a6cf092b7354
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-27 11:23:09 -0500

    Add create_file function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file.
+ * @text_content - NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure (file cannot be created, file cannot be
+ * written, write "fails", etc...)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_EXCL, 600);
+	if (fd < 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		bytes = write(fd, text_content, strlen(text_content));
+		if (bytes == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
