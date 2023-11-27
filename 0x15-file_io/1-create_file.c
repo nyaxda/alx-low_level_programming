539af9c216a3dacaf292e9fcfd37a132f5d883ee
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-27 11:30:48 -0500

    Fix file permissions in create_file function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_EXCL, 600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_EXCL, 0600);
 	if (fd < 0)
+	{
+		if (errno == EEXIST)
+			fd = open(filename, O_WRONLY | O_TRUNC, 0600);
+	}
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
