9dcaa851899cf1c8b2a0e213a4e7d197624866bf
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-27 10:16:59 -0500

    Added C file "0-read_textfile.c"

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout.
+ * @filename: name of the file where the text is read from
+ * @letters: the size in bytes of characters to be read.
+ * Return: actual numbers of letters it could read and print.
+ * if the file cannot be opened of read, return 0,
+ * if filename is NULL, return 0.
+ * if write fails or does not write the expected amounts of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t size, bytes;
+	char *c;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY)
+	if (fd < 0)
+		return (0);
+
+	c = (char*)malloc(letters * sizeof(char));
+	if (c == NULL)
+	{
+		close (fd);
+		return (0);
+	}
+	size = read(fd, c, letters);
+	if (size < 0)
+	{
+		free (c);
+		close (fd);
+		return (0);
+	}
+	c[size] = '\0';
+
+	bytes = write(STDOUT_FILENO, c, size);
+	if (bytes != size)
+	{
+		free (c);
+		close (fd);
+		return (0);
+	}
+	free (c);
+	close (fd);
+
+	return (size);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
