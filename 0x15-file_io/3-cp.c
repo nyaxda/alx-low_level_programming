20653b1276a30911ecb5cb77d5b5f9e9ebd485ed
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-27 14:33:38 -0500

    Fix variable name in file copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ void copier(int stat, int fd, char *filename, char mode);
  */
 int main(int argc, char *argv[])
 {
-	int src, dest, buffer = 1024, wrt, close_src, close_dest;
+	int src, dest, intread = 1024, wrt, close_src, close_dest;
 	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	char buffer[1024];
 
@@ -54,12 +54,12 @@ int main(int argc, char *argv[])
 	copier(src, -1, argv[1], 'O');
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	copier(dest, -1, argv[2], 'W');
-	while (buffer == 1024)
+	while (intread == 1024)
 	{
-		buffer = read(src, buffer, sizeof(buffer));
-		if (buffer == -1)
+		intread = read(src, buffer, sizeof(buffer));
+		if (intread == -1)
 			copier(-1, -1, argv[1], 'O');
-		wrt = write(dest, buffer, buffer);
+		wrt = write(dest, buffer, intread);
 		if (wrt == -1)
 			copier(-1, -1, argv[2], 'W');
 	}
