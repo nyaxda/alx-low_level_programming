9b8f68a37598691c77ad76a1b488ad67fece18da
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-11 14:40:37 -0600

    Betty

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - Returns the node at a given index in a doubly linked list.
+ * get_dnodeint_at_index - Returns the node at a given index
+ * in a doubly linked list.
  * @head: Pointer to the head of the doubly linked list.
  * @index: Index of the node to retrieve.
  *
- * Return: Pointer to the node at the given index, or NULL if the index is out of range.
+ * Return: Pointer to the node at the given index, or NULL if
+ * the index is out of range.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
