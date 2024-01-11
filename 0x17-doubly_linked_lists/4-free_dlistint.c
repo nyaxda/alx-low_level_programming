2cedcbb78d8fa34d28f6ec3ffa84cbbfd4cdd2f8
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-11 14:10:33 -0600

    Added File 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t temp;
+
+	if (temp == NULL)
+		return;
+	while (temp != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
