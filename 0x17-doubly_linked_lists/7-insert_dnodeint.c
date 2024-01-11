8ca6c4995440991afb79c1902df53bfa5191f408
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-11 17:08:27 -0600

    Testing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,50 +1,44 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a
- * given position in a doubly linked list.
- * @h: A pointer to the head of the doubly linked list.
- * @idx: The index at which the new node should be inserted.
- * @n: The value to be stored in the new node.
- *
- * Return: The address of the newly inserted node, or NULL if it fails.
+ * insert_dnodeint_at_index - function to insert node at nth index
+ * @h: double pointer of dlistint_t type to head of linked list
+ * @idx: index to insert node
+ * @n: data for new node
+ * Return: address of new node or NULL if failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *new_node;
-	unsigned int tracker = 0;
+	dlistint_t *new_node, *temp;
 
 	if (h == NULL)
 		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	current = *h;
-	while (current != NULL && tracker < idx)
-	{
-		current = current->next;
-		tracker++;
-	}
+	temp = get_dnodeint_at_index(*h, idx - 1);
+	if (temp == NULL && idx != 0)
+		return (NULL);
 
-	if (tracker != idx)
-	{
-		if (tracker == idx - 1 && current == NULL)
-			return (add_dnodeint_end(h, n));
-		else
-			return (NULL);
-	}
+	if (temp == NULL && idx == 0)
+		return (add_dnodeint(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
-	new_node->next = current;
 
-	if (current != NULL)
-	{
-		new_node->prev = current->prev;
-		current->prev->next = new_node;
-		current->prev = new_node;
-	}
+	new_node->n = n;
+	new_node->next = temp->next;
+	new_node->prev = temp;
+
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	else
+		return (add_dnodeint_end(h, n));
+
+	temp->next = new_node;
+
 	return (new_node);
 }
