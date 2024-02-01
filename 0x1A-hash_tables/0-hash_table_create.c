8640a5db53f5b4072574f119c3bf8649c0543bc4
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-01 06:21:15 -0800

    Fix initialization of hash table array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,4 +14,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_s));
+	if (!new_table->array)
+		return (NULL);
+	
+	for (; i < size; i++)
+		new_table->array[i] = NULL;
+	
+	return (new_table);
 }
