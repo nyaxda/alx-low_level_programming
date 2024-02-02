bfeb25cde3dbdbebbcd5c6d9cf230695674e0606
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-02 04:08:43 -0800

    Fix hash table size assignment

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 
-	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (!new_table->array)
 		return (NULL);
-
+	new_table->size = size;
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 
