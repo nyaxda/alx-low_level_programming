cf93968eeace47c1575771da1ff465cbd501ef5b
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-07 05:23:43 -0700

    Betty Checks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,15 +1,25 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - Searches for a value in an array
+ * using linear search algorithm.
+ *
+ * @array: Pointer to the first element of the array.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The index of the value if found, otherwise -1.
+ */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while(i < size)
-    {
-        printf("Value checked array[%lu] = [%d]\n",i, array[i]);
-        if (array[i] == value)
-            return (i);
-        i++;
-    }
-    return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
