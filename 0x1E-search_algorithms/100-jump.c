2ad01688073a0233300e2c150b28dbe54e9fe76f
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-08 08:18:38 -0700

    refactor: improve jump_search function with optimized implementation

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,62 +1,43 @@
 #include "search_algos.h"
 
 /**
- * min - Returns the minimum of two values
- * @a: The first value
- * @b: The second value
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
  *
- * Return: The minimum value between @a and @b
- */
-size_t min(size_t a, size_t b)
-{
-	if (a > b)
-		return (b);
-	return (a);
-}
-
-/**
- * jump_search - Searches for a value in a
- * sorted array using jump search algorithm
- *
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * @value: Value to search for
- *
- * Return: Index of the value if found, -1 otherwise
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step, prev, start;
+	int index, m, k, prev;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
-	step = sqrt(size);
-	prev = 0;
 
-	while (array[min(step, size) - 1] < value)
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-		prev = step;
-		step += sqrt(size);
-		if (prev >= size)
-			return (-1);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
-	if (prev != 0)
-	{
-		start = prev - (size_t)sqrt(size);
-		printf("Value found between indexes [%lu] and [%lu]\n", start, prev);
-	}
-	else
-	{
-		start = 0;
-		printf("Value found between indexes [0] and [%lu]\n", prev);
-	}
-	while (start < min(step, size))
-	{
-		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-		if (array[start] == value)
-			return (start);
-		start++;
-	}
+
 	return (-1);
 }
